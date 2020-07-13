959bb28a9ecd35f2d23309d1315cfed12cebf014
minhhoang <minhhoang@kms-technology.com>
2020-07-13 11:35:45 +0700

    Update sample grapql code

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var xmlparser = require('express-xml-bodyparser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const xmlparser = require('express-xml-bodyparser');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-var routes = require('./routes/index');
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-var app = express();
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const routes = require('./routes/index');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+server.applyMiddleware({ app });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(xmlparser({trim: false, explicitArray: false}));
+app.use(xmlparser({ trim: false, explicitArray: false }));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -30,7 +48,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     console.log(err);
     res.sendStatus(err.status || 500);
   });
@@ -38,9 +56,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.sendStatus(err.status || 500);
 });
-
 
 module.exports = app;
